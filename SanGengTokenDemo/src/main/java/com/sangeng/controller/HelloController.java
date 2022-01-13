6352c2f36ad2cdf4ae25e941cb3f2863214a9e80
Caesar454905970 <454905970@qq.com>
2022-01-13 17:07:32 +0800

    登录+鉴权

---
+++ SanGengTokenDemo/src/main/java/com/sangeng/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.sangeng.controller;
+
+import com.sangeng.domain.ResponseResult;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    @PreAuthorize("@ex.hasAuthority('system:dept:list')") //注解：执行对应的表达式，返回为true才会继续执行下面的内容
+//    @PreAuthorize("hasAnyAuthority('admin','test','system:dept:list')")
+//    @PreAuthorize("hasRole('system:dept:list')")
+//    @PreAuthorize("hasAnyRole('admin','system:dept:list')")
+    public String hello(){
+        return "hello";
+    }
+
+    @RequestMapping("/testCors")
+    public ResponseResult testCors(){
+        return new ResponseResult(200,"testCors");
+    }
+}
